584c9114f03bdcf64bf1ea5afdcbc89432ea7cb8
Admin <admin@Mac-Admin.local>
2017-01-29 23:27:34 +0300

    clean

---
+++ ObjC2_les1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjC2_les1
+//
+//  Created by Admin on 29.01.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjC2_les1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjC2_les1
+//
+//  Created by Admin on 29.01.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
